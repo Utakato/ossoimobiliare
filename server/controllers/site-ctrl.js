fe41909e3e7592aea3f93168ad235cc77a9e4153
Utakato <copyresearch98@gmail.com>
2022-01-25 14:58:46 +0200

    Change url from localhost to heroku url

--- server/controllers/site-ctrl.js
+++ server/controllers/site-ctrl.js
@@ -6,7 +6,7 @@ const imgs = require("./../DB/images_to_file.js")
 	## Global variables
 ---------------------------------------------------- */
 
-const baseURL = "http://localhost:3000"
+const baseURL = "https://ossoimobiliare.herokuapp.com/"
 
 
 /* --------------------------------------------------
